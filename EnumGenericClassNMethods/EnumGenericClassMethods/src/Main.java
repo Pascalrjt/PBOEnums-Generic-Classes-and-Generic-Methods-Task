0c60ad81485b14551fafb93a86121f0c5f005ac7
Pascal Roger J T <91580977+Pascalrjt@users.noreply.github.com>
2022-11-09 11:37:01 +0700

    Add files via upload

---
+++ EnumGenericClassNMethods/EnumGenericClassMethods/src/Main.java
@@ -0,0 +1,28 @@
+class Personel<P> {
+	P obj;
+
+	Personel(P obj) {
+		this.obj = obj;
+	}
+
+	public P getObject() {
+		return this.obj;
+	}
+}
+
+enum secClearance {
+	Confidentail,
+	Secret,
+	TopSecret
+}
+
+class Main {
+	public static void main(String[] args) {
+
+		Personel<String> sObj = new Personel<String>("Rafi");
+		secClearance clearance = secClearance.Secret;
+
+		System.out.println("Personel name		: " + sObj.getObject());
+		System.out.println("Personal clearance 	: " + clearance);
+	}
+}
